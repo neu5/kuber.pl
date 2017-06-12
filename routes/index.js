341edae7bbf8fcc36c9fbfba5bc3adf336387422
Jakub Kasprzyk <neus90@gmail.com>
2017-06-12 16:01:22 +0200

    Add special colors for days and color legend

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const data = require('../data.json');
 const router = express.Router();
 
 const m = moment();
-const year = m.year();
+const YEAR = m.year();
 
 const months = [
   { daysNum: 31 },
@@ -22,18 +22,46 @@ const months = [
   { daysNum: 31 }
 ];
 
-function isToday(today) {
-  return moment(today, 'YYYY-MM-DD').isSame(m, 'day') ? 'day--today' : '';
+function isSameDay(oneDay, secondDay) {
+  return moment(oneDay, 'YYYY-MM-DD').isSame(secondDay, 'd');
 }
 
 function getDay(i, firstDayOfMonth, shiftedDay, daysNum) {
   return i >= firstDayOfMonth && shiftedDay <= daysNum ? shiftedDay.toString() : '';
 }
 
+
+function getClassNames(row, monthNum, day) {
+  const currentDay = `${YEAR}-${monthNum + 1}-${day}`;
+  let classNames = '';
+
+  if (row > 4) {
+    classNames += 'color__weekend';
+  }
+  // check for today
+  if (isSameDay(`${YEAR}-${monthNum + 1}-${day}`, m)) {
+    classNames += ' color__today';
+  }
+  // check for days defined in external source
+  data.forEach(dataDay => {
+    if ((isSameDay(currentDay, moment(dataDay.maybe, 'DD-MM-YYYY')))) {
+      classNames += ' color__maybe';
+    }
+    if ((isSameDay(currentDay, moment(dataDay.available, 'DD-MM-YYYY')))) {
+      classNames += ' color__available';
+    }
+    if ((isSameDay(currentDay, moment(dataDay.full, 'DD-MM-YYYY')))) {
+      classNames += ' color__full';
+    }
+  });
+
+  return classNames;
+}
+
 function getMonthGrid(month, monthNum, daysToRender = 7, weeksToRender = 6) {
   const grid = [];
   const daysNum = month.daysNum;
-  const firstDayOfMonth = moment([year, monthNum]).format('E');
+  const firstDayOfMonth = moment([YEAR, monthNum]).format('E');
   
   let day = '';
   let i = 1;
@@ -45,7 +73,7 @@ function getMonthGrid(month, monthNum, daysToRender = 7, weeksToRender = 6) {
 
       grid.push({
         day,
-        className: (row > 4 ? 'day--weekend' : '') + ' ' + isToday(`${year}-${monthNum + 1}-${day}`)
+        className: day === '' ? '' : getClassNames(row, monthNum, day)
       });
 
       i++;
@@ -55,7 +83,7 @@ function getMonthGrid(month, monthNum, daysToRender = 7, weeksToRender = 6) {
   return grid;
 }
 
-function getDays(months) {
+function getCalendar(months) {
   return months.map((month, idx) => {
     return Object.assign(month, {
       name: moment.months()[idx],
@@ -64,12 +92,12 @@ function getDays(months) {
   });
 }
 
-const days = getDays(months);
+const calendar = getCalendar(months);
 
 router.get('/', function(req, res) {
   res.render('index', { 
     today: m.format('DD-MM-YYYY'),
-    months: months,
+    calendar: calendar,
   });
 });
 
